71c9b37fc3cabecd4c01b14e37ea7a2b087a3509
IgorFilin <filinigor@yandex.ru>
2024-05-16 23:04:44 +0300

    Двойная привязка через ngModel директиву

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
+import { Test4Component } from './test4/test4.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, TestComponent, Test2Component, Test3Component],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    TestComponent,
+    Test2Component,
+    Test3Component,
+    Test4Component,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
